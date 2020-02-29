bbabd4b234f5fc1d255b131afb059356a76bc8fd
Geopopos <groros2@icloud.com>
2020-02-28 21:35:50 -0500

    set to debug mode

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route("/")
 @app.route("/hello")
 def hello_world():
     return "Hello, World!"
+
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
 
 if __name__ == "__main__":
     app.run()
